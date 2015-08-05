258f5670dde6607b2df8b1310815c1dfe97f11c6
spriggsc <carynspriggs@gmail.com>
2015-08-05 13:03:15 -0700

    Javascript take home
    Take home assignments for days 1 and 2.

---
+++ day1.2/app.js
@@ -0,0 +1,5 @@
+var firstName = prompt("what is your first name?");
+var lastName = prompt("what is your last name?");
+console.log(firstName);
+alert(lastName);
+confirm(firstName + lastName);
---
+++ day2-2/app2.js
@@ -0,0 +1,19 @@
+/**
+ * Created by donaldspriggs on 8/4/15.
+ */
+// <!-- Prompt for a number value (provide a default of 10)
+// Prompt for another number value (provide a default of 10)
+// Convert the prompted values into integers.
+// add the numbers together and alert the user with the outcome. -->
+
+
+var num = 10;
+
+var num2 = 10;
+
+prompt(num);
+prompt(num2);
+
+parseInt(num + num2);
+alert(num + num2);
+
